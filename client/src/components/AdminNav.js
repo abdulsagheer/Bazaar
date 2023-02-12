9b78d036ea7dbfe1b1343b793e3606b6ebf5a558
abdulsagheer <abdulsagheeras29@gmail.com>
2023-02-12 22:49:57 +0530

    indentation

--- client/src/components/AdminNav.js
+++ client/src/components/AdminNav.js
@@ -1,17 +1,23 @@
-import {useDispatch} from "react-redux"
+import { useDispatch } from "react-redux";
 import { logout } from "../store/reducers/authReducer";
-const AdminNav = ({openSidebar}) => {
-    const dispatch = useDispatch();
-    const adminLogout = () => {
-        dispatch(logout('admin-token'));
-    }
-    return(
-     <nav className="fixed left-0 sm:left-64 top-4 right-0 mx-4">
-      <div className="bg-gray-800 w-full flex justify-between sm:justify-end items-center p-4">
-      <i className="bi bi-filter-left text-white text-2xl cursor-pointer sm:hidden block" onClick={openSidebar}></i>
-       <button className="py-2 px-4 bg-indigo-600 text-white rounded-md capitalize" onClick={adminLogout}>logout</button>
-      </div>
-     </nav>
-    )
-}
+const AdminNav = ({ openSidebar }) => {
+	const dispatch = useDispatch();
+	const adminLogout = () => {
+		dispatch(logout("admin-token"));
+	};
+	return (
+		<nav className="fixed left-0 right-0 mx-4 sm:left-64 top-4">
+			<div className="flex items-center justify-between w-full p-4 bg-gray-800 sm:justify-end">
+				<i
+					className="block text-2xl text-white cursor-pointer bi bi-filter-left sm:hidden"
+					onClick={openSidebar}></i>
+				<button
+					className="px-4 py-2 text-white capitalize bg-indigo-600 rounded-md"
+					onClick={adminLogout}>
+					logout
+				</button>
+			</div>
+		</nav>
+	);
+};
 export default AdminNav;
